48aad7e78fe55879506b36dd0ef90797146ee4d3
davidgoweb <maykhel.david@gmail.com>
2025-12-01 22:01:50 +0700

    feat: add build and start production script

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  mode: 'production',
+  optimization: {
+    minimize: true,
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
